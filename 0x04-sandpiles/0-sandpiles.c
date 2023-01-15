0c7cd52e56e835af9b6c9e79f4121ba4cd108845
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-01-15 21:13:07 +0100

    update

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -20,3 +20,22 @@ static void print_grid(int grid[3][3])
 		printf("\n");
 	}
 }
+
+/**
+  * copy - copy of two sandpiles.
+  * @scr: 2D array.
+  * @des: 2D array.
+  */
+
+void copy(int scr[3][3], int des[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3;i++)
+{
+		for (j =0; j < 3;j++)
+		{
+			des[i][j] = scr[i][j];
+	}
+	}
+}
