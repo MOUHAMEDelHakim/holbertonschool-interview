b01e7fc2c7117496fdc05919f2dcf321478377ba
Mohamed El Hakim <mohamed@MacBook-Air-de-Chiraz.local>
2023-01-15 21:07:59 +0100

    sandpiles

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,11 @@
-#ifndef _SANDPILES_H_
-#define _SANDPILES_H_
+#ifndef __SANDPILES__LIB__
+#define __SANDPILES__LIB__
 
 #include <stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int verif(int grid[3][3]);
+static void print_grid(int grid[3][3]);
 
-#endif /* _SANDPILES_H_ */
+#endif
